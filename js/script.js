323ea85860f40f12d68c3bfd2a72a099ca99b9c8
Valter <valterinside@gmail.com>
2016-06-23 17:50:48 -0300

    busca tabela com jquery

--- js/script.js
+++ js/script.js
@@ -297,7 +297,6 @@ function buscaPorId(){
 			}
 			tr[i].style.display = "";
 			//tr = row.innerHTML;
-			
 			rowCount--;
 			i--;
 		}
@@ -313,3 +312,21 @@ function buscaPorId(){
 		}
 	}
 }
+
+$(function(){
+    $(".input-search").keyup(function(){
+        //pega o css da tabela 
+        var tabela = $(this).attr('alt');
+        if( $(this).val() != ""){
+            $("."+tabela+" tbody>tr").hide();
+            $("."+tabela+" td:contains-ci('" + $(this).val() + "')").parent("tr").show();
+        } else{
+            $("."+tabela+" tbody>tr").show();
+        }
+    }); 
+});
+$.extend($.expr[":"], {
+    "contains-ci": function(elem, i, match, array) {
+        return (elem.textContent || elem.innerText || $(elem).text() || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+    }
+});
